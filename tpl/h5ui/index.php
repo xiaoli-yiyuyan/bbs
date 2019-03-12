181994f49e000873e1190c09c697eca056520e08
稀饭 <675567585@qq.com>
2019-03-12 16:12:06 +0800

    添加首页图片

--- tpl/h5ui/index.php
+++ tpl/h5ui/index.php
@@ -1,6 +1,8 @@
 <?php component('/components/common/header?title=安米社区-专注于手机网站建设' ); ?>
 <?php component('/components/common/header_menu_nav?title=安米社区' ); ?>
-
+<div class="img_bar">
+    <img src="/static/images/ad_bar.jpg" alt="">
+</div>
 <?php $list = source('Model/Forum/getList'); ?>
 <div class="list img_list">
 <?php foreach($list as $item) { ?>
