0ecd521960c5274551cba45a3d51c14664f16be9
稀饭 <675567585@qq.com>
2019-07-23 13:11:41 +0800

    '版本号'

--- comm/core/IamVersion.php
+++ comm/core/IamVersion.php
@@ -3,8 +3,8 @@ namespace comm\core;
 
 class IamVersion
 {
-    public static $version = '2.1.5';
-    public static $datetime = '2019-06-03';
+    public static $version = '2.2.0';
+    public static $datetime = '2019-07-23';
 
     public static function getVersion()
     {
