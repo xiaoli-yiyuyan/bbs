21258867cb160784c2f34b390e3bb2b244069839
稀饭 <675567585@qq.com>
2019-09-03 13:40:16 +0800

    升级兼容

--- comm/core/IamVersion.php
+++ comm/core/IamVersion.php
@@ -3,7 +3,7 @@ namespace comm\core;
 
 class IamVersion
 {
-    public static $version = '2.3.0';
+    public static $version = '2.3.2';
     public static $datetime = '2019-09-03';
 
     public static function getVersion()
