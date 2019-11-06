9e3e7a257a44d2134a89b55bacfbfd646aeabbb1
稀饭 <675567585@qq.com>
2019-11-06 10:58:00 +0800

    '修复bug'

--- comm/core/IamVersion.php
+++ comm/core/IamVersion.php
@@ -3,7 +3,7 @@ namespace comm\core;
 
 class IamVersion
 {
-    public static $version = '2.3.16';
+    public static $version = '2.3.17';
     public static $datetime = '2019-11-06';
 
     public static function getVersion()
