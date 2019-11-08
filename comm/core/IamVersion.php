f1f1c6e56f55e72a8c965c2c01194be9315b49b6
稀饭 <675567585@qq.com>
2019-11-08 13:59:47 +0800

    '更新回复翻页bug'

--- comm/core/IamVersion.php
+++ comm/core/IamVersion.php
@@ -3,8 +3,8 @@ namespace comm\core;
 
 class IamVersion
 {
-    public static $version = '2.3.18';
-    public static $datetime = '2019-11-06';
+    public static $version = '2.3.19';
+    public static $datetime = '2019-11-08';
 
     public static function getVersion()
     {
